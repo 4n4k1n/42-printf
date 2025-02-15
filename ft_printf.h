e8b0ef9e1eee8d89ca0a2f7b83ab6688191aff78
4n4k1n <anakin@pregitzer.de>
2025-02-15 16:20:13 +0100

    added the printf prototype

--- ft_printf.h
+++ ft_printf.h
@@ -1,14 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/15 15:33:39 by anakin            #+#    #+#             */
+/*   Updated: 2025/02/15 16:19:41 by anakin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+# define FT_PRINTF_H
 
-#include <unistd.h>
-#include <stdarg.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdlib.h>
 
-int	ft_print_char(char c);
-int	ft_print_str(char *str);
-int ft_print_ptr(void *ptr);
-int	ft_print_nbr(int n);
-int	ft_print_usgnd_nbr(unsigned int nbr);
-int	ft_print_hex(int nbr, char format);
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_putptr(void *ptr);
+int	ft_putnbr(int n);
+int	ft_putunbr(unsigned int nbr);
+int	ft_puthex(int nbr, char format);
+int	ft_printf(const char *format, ...);
 
 #endif
