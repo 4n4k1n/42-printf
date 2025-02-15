d00da9c21df055bfc17b1867826b692446e44531
4n4k1n <anakin@pregitzer.de>
2025-02-15 16:17:17 +0100

    corrected the function names

--- ft_printf.c
+++ ft_printf.c
@@ -6,10 +6,9 @@
 /*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 18:00:20 by anakin            #+#    #+#             */
-/*   Updated: 2025/02/14 21:54:37 by anakin           ###   ########.fr       */
+/*   Updated: 2025/02/15 16:12:28 by anakin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "ft_printf.h"
 #include <stdio.h>
@@ -17,17 +16,17 @@
 int	print_formats(const char *format, va_list *args)
 {
 	if (*format == 'c')
-		return (ft_print_char(va_arg(*args, int)));
+		return (ft_putchar(va_arg(*args, int)));
 	else if (*format == 's')
-		return (ft_print_str(va_arg(*args, char *)));
+		return (ft_putstr(va_arg(*args, char *)));
 	else if (*format == 'p')
-		return (ft_print_ptr(va_arg(*args, void *)));
+		return (ft_putptr(va_arg(*args, void *)));
 	else if (*format == 'd' || *format == 'i')
-		return (ft_print_nbr(va_arg(*args, int)));
+		return (ft_putnbr(va_arg(*args, int)));
 	else if (*format == 'u')
-		return (ft_print_usgnd_nbr(va_arg(*args, unsigned int)));
+		return (ft_putunbr(va_arg(*args, unsigned int)));
 	else if (*format == 'x' || *format == 'X')
-		return (ft_print_hex(va_arg(*args, int), *format));
+		return (ft_puthex(va_arg(*args, int), *format));
 	else if (*format == '%')
 	{
 		write(1, "%", 1);
@@ -38,8 +37,8 @@ int	print_formats(const char *format, va_list *args)
 
 int	ft_printf(const char *format, ...)
 {
-	va_list args;
-	int count;
+	va_list	args;
+	int		count;
 
 	count = 0;
 	va_start(args, format);
@@ -51,18 +50,31 @@ int	ft_printf(const char *format, ...)
 			count += print_formats(format, &args);
 		}
 		else
-			write(1, format, 1);
+			count += write(1, format, 1);
 		format++;
 	}
 	va_end(args);
 	return (count);
 }
 
-#include <stdio.h>
-int main(void)
+int	main(void)
 {
-	int nbr = 42;
-	
-	ft_printf("%p\n", &nbr);
+	int	nbr;
+	int	count;
+
+	nbr = 42;
+	count = ft_printf(
+		"pointer    = %p\n" \
+		"int(d)     = %d\n" \
+		"int(i)     = %i\n" \
+		"char       = %c\n" \
+		"str        = %s\n" \
+		"str (null) = %s\n" \
+		"hex(low)   = %x\n" \
+		"hex(up)    = %X\n" \
+		"unsinged   = %u\n" \
+		"sign       = %%\n",
+		&nbr, nbr, nbr, 'c', "test test", NULL, nbr, nbr, (unsigned int)nbr);
+		ft_printf("count = %d\n", count);
 	return (0);
 }
