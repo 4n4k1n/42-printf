fa0dfa1dea94b0e7b72aec4a12b3b70e872c06f7
4n4k1n <anakin@pregitzer.de>
2025-02-19 13:55:13 +0100

    added the struct

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 15:33:39 by anakin            #+#    #+#             */
-/*   Updated: 2025/02/15 16:19:41 by anakin           ###   ########.fr       */
+/*   Updated: 2025/02/17 22:58:57 by anakin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,19 @@
 # include <unistd.h>
 # include <stdarg.h>
 # include <stdlib.h>
+
+typedef struct s_flags
+{
+	int	minus_flag;
+	int	plus_flag;
+	int	number_flag;
+	int	hash_flag;
+	int	dot_flag;
+	int	zero_flag;
+	int	space_flag;
+	int	data_tpye;
+	int	depth;
+}	t_flags;
 
 int	ft_putchar(char c);
 int	ft_putstr(char *str);
