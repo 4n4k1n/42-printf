5e007caf3a11448e7c94a2fefb901ba646e53233
4n4k1n <anakin@pregitzer.de>
2025-02-23 12:00:36 +0100

    corrected the issue with the negativ values

--- ft_puthex.c
+++ ft_puthex.c
@@ -6,33 +6,30 @@
 /*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 19:15:11 by anakin            #+#    #+#             */
-/*   Updated: 2025/02/15 15:41:39 by anakin           ###   ########.fr       */
+/*   Updated: 2025/02/23 11:53:48 by anakin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_hex_base(long long int nbr, char *base)
+static int	ft_hex_base(unsigned int nbr, char *base)
 {
-	int	negativ;
+	int	result;
 
-	negativ = 0;
-	if (nbr < 0)
-	{
-		write(1, "-", 1);
-		nbr = nbr * (-1);
-		negativ = 1;
-	}
+	result = 0;
 	if (nbr >= 16)
-		negativ += ft_hex_base(nbr / 16, base);
+		result += ft_hex_base(nbr / 16, base);
 	write(1, &base[nbr % 16], 1);
-	return (negativ + 1);
+	return (result + 1);
 }
 
 int	ft_puthex(int nbr, char format)
 {
+	unsigned int	cpy_nbr;
+
+	cpy_nbr = (unsigned int)nbr;
 	if (format == 'x')
-		return (ft_hex_base(nbr, "0123456789abcdef"));
+		return (ft_hex_base(cpy_nbr, "0123456789abcdef"));
 	else
-		return (ft_hex_base(nbr, "0123456789ABCDEF"));
+		return (ft_hex_base(cpy_nbr, "0123456789ABCDEF"));
 }
