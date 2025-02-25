ecd524483635f950878862b7c6c950d6f93e44db
4n4k1n <anakin@pregitzer.de>
2025-02-25 11:09:01 +0100

    added functions

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 15:33:39 by anakin            #+#    #+#             */
-/*   Updated: 2025/02/22 10:26:37 by anakin           ###   ########.fr       */
+/*   Updated: 2025/02/25 10:42:59 by anakin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,20 @@ typedef struct s_flags
 	int		zero;
 	int		hash;
 	int		space;
+	int		dot;
+	int		width;
 	char	type;
 }	t_flags;
 
-int	ft_putchar(char c);
-int	ft_putstr(char *str);
-int	ft_putptr(void *ptr);
-int	ft_putnbr(int n);
-int	ft_putunbr(unsigned int nbr);
-int	ft_puthex(int nbr, char format);
-int	ft_printf(const char *format, ...);
+int		ft_putchar(char c);
+int		ft_putstr(char *str);
+int		ft_putptr(void *ptr);
+int		ft_putnbr(int n);
+int		ft_putunbr(unsigned int nbr);
+int		ft_puthex(int nbr, char format, t_flags data);
+int		ft_printf(const char *format, ...);
+void	ft_prpare_flags(t_flags *data);
+int		ft_get_flags(t_flags *data, const char *s);
+int		ft_is_specifier(const char c);
 
 #endif
