5339b43b3bfef33016e99d4854f5d0ab1299f5e5
4n4k1n <anakin@pregitzer.de>
2025-02-25 11:11:45 +0100

    added the prefix flag

--- ft_puthex.c
+++ ft_puthex.c
@@ -6,7 +6,7 @@
 /*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 19:15:11 by anakin            #+#    #+#             */
-/*   Updated: 2025/02/23 11:53:48 by anakin           ###   ########.fr       */
+/*   Updated: 2025/02/24 21:51:48 by anakin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,19 @@ static int	ft_hex_base(unsigned int nbr, char *base)
 	return (result + 1);
 }
 
-int	ft_puthex(int nbr, char format)
+int	ft_puthex(int nbr, char format, t_flags data)
 {
 	unsigned int	cpy_nbr;
+	int				count;
 
+	count = 0;
+	if (data.hash && format == 'x')
+		count = write(1, "0x", 2);
+	else if (data.hash)
+		count = write(1, "0X", 2);
 	cpy_nbr = (unsigned int)nbr;
 	if (format == 'x')
-		return (ft_hex_base(cpy_nbr, "0123456789abcdef"));
+		return (ft_hex_base(cpy_nbr, "0123456789abcdef") + count);
 	else
-		return (ft_hex_base(cpy_nbr, "0123456789ABCDEF"));
+		return (ft_hex_base(cpy_nbr, "0123456789ABCDEF") + count);
 }
