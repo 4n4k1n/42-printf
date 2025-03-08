b2e91b3592238ec90a2a264b3184a1a6b02bfba0
4n4k1n <anakin@pregitzer.de>
2025-03-08 18:30:16 +0100

    added new functions

--- ft_printf.h
+++ ft_printf.h
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 15:33:39 by anakin            #+#    #+#             */
-/*   Updated: 2025/02/22 10:26:37 by anakin           ###   ########.fr       */
+/*   Updated: 2025/03/08 17:48:37 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <unistd.h>
 # include <stdarg.h>
 # include <stdlib.h>
+# include <unistd.h>
 
 typedef struct s_flags
 {
@@ -25,14 +25,17 @@ typedef struct s_flags
 	int		hash;
 	int		space;
 	char	type;
-}	t_flags;
+}			t_flags;
 
-int	ft_putchar(char c);
-int	ft_putstr(char *str);
-int	ft_putptr(void *ptr);
-int	ft_putnbr(int n);
-int	ft_putunbr(unsigned int nbr);
-int	ft_puthex(int nbr, char format);
-int	ft_printf(const char *format, ...);
+int			*get_error(void);
+void		set_error(int new_status);
+
+int			ft_putchar(char c);
+int			ft_putstr(char *s);
+int			ft_putptr(void *ptr);
+int			ft_putnbr(int n);
+int			ft_putunbr(unsigned int nbr);
+int			ft_puthex(int nbr, char format);
+int			ft_printf(const char *format, ...);
 
 #endif
