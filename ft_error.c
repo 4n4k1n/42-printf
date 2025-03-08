ea3059f09b3493fc3edb3ad34ec607ceec27ca96
4n4k1n <anakin@pregitzer.de>
2025-03-08 18:27:44 +0100

    function to catch a write error

--- ft_error.c
+++ ft_error.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 17:22:59 by apregitz          #+#    #+#             */
-/*   Updated: 2025/03/08 17:39:10 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/03/08 17:48:29 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,26 +23,4 @@ void	set_error(int new_status)
 
 	status = get_error();
 	*status = new_status;
-}
-
-int	ft_putchar_er(char c)
-{
-	int	status;
-
-	if (!get_error)
-	{
-		if (write(1, &c, 1) < 1)
-			set_error(1);
-	}
-	return (1);
-}
-
-int	ft_putchar_str(char *s)
-{
-	int	count;
-
-	count = -1;
-	while (s[++count])
-		ft_putchar_er(s[count]);
-	return (count);
 }
